8706b92bb1e9eb8501c118245eff307f736d33b2
alifshah <alifhasanshah@gmail.com>
2022-08-20 10:26:46 +0600

    install tailwind and daisyui and configure it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Github User Finder</h1>
-    </div>
+    <>
+     <Router>
+      <div className='flex flex-col justify-between h-screen'>
+      <Navbar />
+      <main>
+        <div>content</div>
+      </main>
+      </div>
+      <Routes>
+        <Route/>
+      </Routes>
+      </Router>
+    </>
   );
 }
 
